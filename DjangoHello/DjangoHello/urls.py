ee47decde69dc7d6fc2dd52ca266cf7a8a4739e7
marcoarc01 <marcowin122@gmail.com>
2025-02-22 11:34:56 -0300

    commit

--- DjangoHello/DjangoHello/urls.py
+++ DjangoHello/DjangoHello/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import include, path 
+from DjangoApp import views
 
 urlpatterns = [ 
-    path('app/', include('DjangoApp.urls')), 
+    path('', views.index, name='index'), 
     path('admin/', admin.site.urls), 
 ] 
